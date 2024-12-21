1e738391721983a03995e14a115d23388afc8e66
Mujeeb-Siddique <mujeebsiddique9@gmail.com>
2024-12-21 23:42:27 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import CurrencyConvertor from './components/CurrencyConvertor'
+
+const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-200 flex flex-col items-center justify-center'>
+        <div className='container'>
+           <CurrencyConvertor/>
+        </div>
+    </div>
+  )
+}
+
+export default App
